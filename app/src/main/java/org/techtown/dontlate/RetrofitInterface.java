d0d7b5295a507f37a57977d109392c3dc5b2bab0
연운 <3yws@naver.com>
2021-08-06 10:15:47 +0900

    카카오지도 api 삭제(서연운)
    카카오지도 api 삭제 및  Manifest 파일 수정

--- app/src/main/java/org/techtown/dontlate/RetrofitInterface.java
+++ app/src/main/java/org/techtown/dontlate/RetrofitInterface.java
@@ -1,12 +1,5 @@
 package org.techtown.dontlate;
 
-import org.techtown.dontlate.model.CategorySearch;
-import org.techtown.dontlate.model.CoordRegionInfo;
-import org.techtown.dontlate.model.KeywordSearch;
-import org.techtown.dontlate.model.SearchingAddress;
-import org.techtown.dontlate.model.TranslateAddress;
-import org.techtown.dontlate.model.TranslateCoord;
-
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
@@ -14,21 +7,5 @@ import retrofit2.http.Query;
 
 public interface RetrofitInterface {
 
-    @GET("v2/local/search/address.json")
-    Call<SearchingAddress> AddressInfo(@Header("Authorization") String authorization, @Query("query") String query);
 
-    @GET("v2/local/geo/coord2regioncode.json")
-    Call<CoordRegionInfo> RegionInfo(@Header("Authorization") String authorization, @Query("x") String x, @Query("y") String y);
-
-    @GET("v2/local/geo/coord2address.json")
-    Call<TranslateAddress> CoordInfo(@Header("Authorization") String authorization, @Query("x") String x, @Query("y") String y);
-
-    @GET("v2/local/geo/transcoord.json")
-    Call<TranslateCoord> CoordxyInfo(@Header("Authorization") String authorization, @Query("x") Double x, @Query("y") Double y,@Query("input_coord") String input_coord, @Query("output_coord") String output_coord);
-
-    @GET("v2/local/search/keyword.json")
-    Call<KeywordSearch> KeplaceInfo(@Header("Authorization") String authorization, @Query("query") String query);
-
-    @GET("v2/local/search/category.json")
-    Call<CategorySearch> CaplaceInfo(@Header("Authorization") String authorization, @Query("category_group_code") String category_group_code, @Query("radius") Integer radius);
 }
--- app/src/main/java/org/techtown/dontlate/navigationss.java
+++ app/src/main/java/org/techtown/dontlate/navigationss.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,37 +19,19 @@ import androidx.fragment.app.Fragment;
 
 import com.skt.Tmap.TMapMarkerItem;
 import com.skt.Tmap.TMapPoint;
-import com.skt.Tmap.TMapTapi;
 import com.skt.Tmap.TMapView;
-
-import net.daum.mf.map.api.MapPOIItem;
-import net.daum.mf.map.api.MapPoint;
-import net.daum.mf.map.api.MapView;
-
-
-import org.techtown.dontlate.model.CategorySearch;
-import org.techtown.dontlate.model.CoordRegionInfo;
-import org.techtown.dontlate.model.KeywordSearch;
-import org.techtown.dontlate.model.SearchingAddress;
-import org.techtown.dontlate.model.TranslateAddress;
-import org.techtown.dontlate.model.TranslateCoord;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class navigationss extends Fragment {
 
 
     private View view;
 
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
+
 
     private RetrofitClient retrofitClient;
     private RetrofitInterface retrofitInterface;
 
-    private String API_KEY = "KakaoAK 8ae57c6ab583cbe890979e12b4c0315a";
+    private String App_Key = "l7xxddf8547d834c4053946c4a168738d92f";
 
 
     @Nullable
@@ -59,25 +40,20 @@ public class navigationss extends Fragment {
 
         View v = inflater.inflate(R.layout.navigations, container, false);
 
-        /*MapView mapView = new MapView(getActivity());
-
-        ViewGroup mapViewContainer = (ViewGroup) v.findViewById(R.id.map_view);
-        mapViewContainer.addView(mapView);*/
-
-//        Context mFcontext = getActivity();
+        Context mFcontext = getActivity();
 
 
         LinearLayout linearLayoutTmap = (LinearLayout) v.findViewById(R.id.linearLayoutTmap);
         TMapView tMapView = new TMapView(getActivity());
 
-        tMapView.setSKTMapApiKey("l7xxddf8547d834c4053946c4a168738d92f");
+        tMapView.setSKTMapApiKey(App_Key);
         linearLayoutTmap.addView(tMapView);
 
 //        TMapMarkerItem markerItem1 = new TMapMarkerItem();
 //
 //        TMapPoint tMapPoint1 = new TMapPoint(37.570841, 126.985302); // SKT타워
-
-// 마커 아이콘
+//
+// //마커 아이콘
 //        Bitmap bitmap = BitmapFactory.decodeResource(mFcontext.getResources(), R.drawable.pin_r_m_a);
 //
 //        markerItem1.setIcon(bitmap); // 마커 아이콘 지정
@@ -86,141 +62,15 @@ public class navigationss extends Fragment {
 //        markerItem1.setName("SKT타워"); // 마커의 타이틀 지정
 //        tMapView.addMarkerItem("markerItem1", markerItem1); // 지도에 마커 추가
 //
-//        tMapView.setCenterPoint( 126.985302, 37.570841 );
+//        tMapView.setCenterPoint( 126.985302, 37.570841 ); //정상 작동 확인 완료
 
 
 
-        /*callCoordRegionItems();
-        callAddressItems();
-        callTranslateAddressItems();
-        callTranslateCoordItems();
-        callKeywordSearchItems();
-        callCategorySearchItems();
 
 
-        // 중심점 변경 - 예제 좌표는 서울 남산
-        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304), true);
-
-        // 줌 레벨 변경
-        mapView.setZoomLevel(4, true);
-
-        //마커 찍기
-        MapPoint MARKER_POINT = MapPoint.mapPointWithGeoCoord(37.54892296550104, 126.99089033876304);//
-        MapPOIItem marker = new MapPOIItem();
-        marker.setItemName("요기");
-        marker.setTag(0);
-        marker.setMapPoint(MARKER_POINT);
-        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
-        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
-
-
-        mapView.addPOIItem(marker);*/
 
 
         return v;
     }
-
- /*   public void callCoordRegionItems() {
-        retrofitClient = RetrofitClient.getInstance();
-        retrofitInterface = RetrofitClient.getRetrofitInterface();
-        retrofitInterface.RegionInfo(API_KEY, "127.10459896729914","37.40269721785548" ).enqueue(new Callback<CoordRegionInfo>() {
-            @Override
-            public void onResponse(Call<CoordRegionInfo> call, Response<CoordRegionInfo> response) {
-                CoordRegionInfo coordRegionInfo = response.body();
-                Log.d("test", coordRegionInfo.getCoordRegionInfoItems().get(0).getAddressName());
-            }
-
-            @Override
-            public void onFailure(Call<CoordRegionInfo> call, Throwable t) {
-                Log.d("test", t.toString());
-            }
-        });
-    }
-
-    public void callAddressItems() {
-        retrofitClient = RetrofitClient.getInstance();
-        retrofitInterface = RetrofitClient.getRetrofitInterface();
-        retrofitInterface.AddressInfo(API_KEY, "서울특별시 강동구 풍성로 128").enqueue(new Callback<SearchingAddress>() {
-            @Override
-            public void onResponse(Call<SearchingAddress> call, Response<SearchingAddress> response) {
-                SearchingAddress searchingAddress = response.body();
-                Log.d("testAddress", searchingAddress.getSearchingAddressItems().get(0).getAddressName());
-            }
-
-            @Override
-            public void onFailure(Call<SearchingAddress> call, Throwable t) {
-                Log.d("testAddress", t.toString());
-            }
-        });
-    }
-
-    public void callTranslateAddressItems() {
-        retrofitClient = RetrofitClient.getInstance();
-        retrofitInterface = RetrofitClient.getRetrofitInterface();
-        retrofitInterface.CoordInfo(API_KEY,"127.423084873712", "37.0789561558879").enqueue(new Callback<TranslateAddress>() {
-            @Override
-            public void onResponse(Call<TranslateAddress> call, Response<TranslateAddress> response) {
-                TranslateAddress translateAddress = response.body();
-                Log.d("testCoord", translateAddress.getTranslateAddressItems().get(0).getAddress().getAddressName());
-            }
-
-            @Override
-            public void onFailure(Call<TranslateAddress> call, Throwable t) {
-                Log.d("testCoord", t.toString());
-            }
-        });
-    }
-
-    public void callTranslateCoordItems() {
-        retrofitClient = RetrofitClient.getInstance();
-        retrofitInterface = RetrofitClient.getRetrofitInterface();
-        retrofitInterface.CoordxyInfo(API_KEY, 160710.37729270622, -4388.879299157299,"WTM", "WGS84").enqueue(new Callback<TranslateCoord>() {
-            @Override
-            public void onResponse(Call<TranslateCoord> call, Response<TranslateCoord> response) {
-                TranslateCoord translateCoord = response.body();
-                Log.d("testCoordxy", translateCoord.getTranslateCoordItems().get(0).getX().toString());
-                Log.d("testCoordxy", translateCoord.getTranslateCoordItems().get(0).getY().toString());
-            }
-
-            @Override
-            public void onFailure(Call<TranslateCoord> call, Throwable t) {
-                Log.d("testCoordxy", t.toString());
-            }
-        });
-    }
-
-    public void callKeywordSearchItems() {
-        retrofitClient = RetrofitClient.getInstance();
-        retrofitInterface = RetrofitClient.getRetrofitInterface();
-        retrofitInterface.KeplaceInfo(API_KEY, "카카오프렌즈").enqueue(new Callback<KeywordSearch>() {
-            @Override
-            public void onResponse(Call<KeywordSearch> call, Response<KeywordSearch> response) {
-                KeywordSearch keywordSearch = response.body();
-                Log.d("testKplace", keywordSearch.getKeywordSearchItems().get(0).getAddressName());
-            }
-
-            @Override
-            public void onFailure(Call<KeywordSearch> call, Throwable t) {
-                Log.d("testKplace", t.toString());
-            }
-        });
-    }
-
-    public void callCategorySearchItems() {
-        retrofitClient = RetrofitClient.getInstance();
-        retrofitInterface = RetrofitClient.getRetrofitInterface();
-        retrofitInterface.CaplaceInfo(API_KEY, "PM9", 20000).enqueue(new Callback<CategorySearch>() {
-            @Override
-            public void onResponse(Call<CategorySearch> call, Response<CategorySearch> response) {
-                CategorySearch categorySearch = response.body();
-                Log.d("testCplace", categorySearch.getCategorySearchItems().get(0).getAddressName());
-            }
-
-            @Override
-            public void onFailure(Call<CategorySearch> call, Throwable t) {
-                Log.d("testCplace", t.toString());
-            }
-        });
-    }*/
 
 }
