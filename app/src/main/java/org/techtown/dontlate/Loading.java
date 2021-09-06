caa066e75e30bedc5480a168101e0452dceb567f
기훈 <djarlgnsskfk@gmail.com>
2021-09-06 15:31:48 +0900

    xml 파일 추가(엄기훈)

---
+++ app/src/main/java/org/techtown/dontlate/Loading.java
@@ -0,0 +1,23 @@
+package org.techtown.dontlate;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Loading extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading);
+        startLoading();
+    }
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
+    }
+}
