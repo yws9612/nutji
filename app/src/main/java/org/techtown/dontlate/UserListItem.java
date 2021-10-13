1c0c1d66c68c198c3f540c5eae333fc758ef14f0
허진영 <a23270978@gmail.com>
2021-10-14 00:06:44 +0900

    파이어베이스 재업 -허-

---
+++ app/src/main/java/org/techtown/dontlate/UserListItem.java
@@ -0,0 +1,23 @@
+package org.techtown.dontlate;
+
+public class UserListItem {
+
+    private String placeName;
+    private String address;
+
+    public String getplaceName() {
+        return placeName;
+    }
+
+    public void setplaceName(String placeName) {
+        this.placeName = placeName;
+    }
+
+    public String getaddress() {
+        return address;
+    }
+
+    public void setaddress(String address) {
+        this.address = address;
+    }
+}
