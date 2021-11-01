2b90d69e14e88d31e07d27366101b77b63d82c4b
서연운 <3yws@naver.com>
2021-11-01 17:33:20 +0900

    파이어베이스 예제 삭제 -서-

--- app/src/main/java/org/techtown/dontlate/scheduless.java
+++ app/src/main/java/org/techtown/dontlate/scheduless.java
@@ -27,83 +27,15 @@ public class scheduless extends Fragment {
 
     private View view;
 
-    EditText et_user_name,et_user_email;
-    Button btn_save;
-
-    private DatabaseReference mDatabase;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.schedules, container, false);
 
-
-        et_user_name = view.findViewById(R.id.et_user_name);
-        et_user_email = view.findViewById(R.id.et_user_email);
-        btn_save = view.findViewById(R.id.btn_save);
-        //firebase 정의
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-
-        readUser();
-
-        btn_save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String getUserName = et_user_name.getText().toString();
-                String getUserEmail = et_user_email.getText().toString();
-
-                //hashmap 만들기
-                HashMap result = new HashMap<>();
-                result.put("name", getUserName);
-                result.put("email", getUserEmail);
-
-                writeNewUser("1",getUserName,getUserEmail);
-
-            }
-        });
-
         return view;
     }
 
-    private void writeNewUser(String userId, String name, String email) {
-        User user = new User(name, email);
 
-        mDatabase.child("users").child(userId).setValue(user)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        // Write was successful!
-                        Toast.makeText(getContext(), "저장을 완료했습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        // Write failed
-                        Toast.makeText(getContext(), "저장을 실패했습니다.", Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-    }
-    private void readUser(){
-        mDatabase.child("users").child("1").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                // Get Post object and use the values to update the UI
-                if(dataSnapshot.getValue(User.class) != null){
-                    User post = dataSnapshot.getValue(User.class);
-                    Log.w("FireBaseData", "getData" + post.toString());
-                } else {
-                    Toast.makeText(getContext(), "데이터 없음...", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                // Getting Post failed, log a message
-                Log.w("FireBaseData", "loadPost:onCancelled", databaseError.toException());
-            }
-        });
-    }
 }
 
