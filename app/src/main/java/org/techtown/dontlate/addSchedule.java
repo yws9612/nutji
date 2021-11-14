3b4e5f59256ca66af9dd87e661ec17c29bdcbb1f
허진영 <a23270978@gmail.com>
2021-11-15 02:24:25 +0900

    UI 수정 -허-
    일정 추가 TimePicker Dialog → Spinner

--- app/src/main/java/org/techtown/dontlate/addSchedule.java
+++ app/src/main/java/org/techtown/dontlate/addSchedule.java
@@ -19,62 +19,26 @@ public class addSchedule extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addschedule);
 
-        final TextView start = (TextView) findViewById(R.id.Start);
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar mcurrentTime = Calendar.getInstance();
-                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-                int minute = mcurrentTime.get(Calendar.MINUTE);
-                TimePickerDialog mTimePicker;
-                mTimePicker = new TimePickerDialog(addSchedule.this, new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        String state = "AM";
+        TimePicker startpicker = (TimePicker) findViewById(R.id.startPicker);
+        startpicker.setIs24HourView(true);
 
-                        if(selectedHour > 12){
-                            selectedHour -= 12;
-                            state = "PM";
-                        }
-                        start.setText(state + " " + selectedHour + "시 " + selectedMinute + "분");
-                    }
-                }, hour, minute, false);
-                mTimePicker.setTitle("시간 선택");
-                mTimePicker.show();
-            }
-        });
+        TimePicker endpicker = (TimePicker) findViewById(R.id.endPicker);
+        endpicker.setIs24HourView(true);
 
-        final TextView end = (TextView) findViewById(R.id.End);
-        end.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar mcurrentTime = Calendar.getInstance();
-                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
-                int minute = mcurrentTime.get(Calendar.MINUTE);
-                TimePickerDialog mTimePicker;
-                mTimePicker = new TimePickerDialog(addSchedule.this, new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        String state = "AM";
-
-                        if(selectedHour > 12){
-                            selectedHour -= 12;
-                            state = "PM";
-                        }
-                        end.setText(state + " " + selectedHour + "시 " + selectedMinute + "분");
-                    }
-                }, hour, minute, false);
-                mTimePicker.setTitle("시간 선택");
-                mTimePicker.show();
-            }
-        });
 
         Button okay = (Button) findViewById(R.id.Okay);
         okay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 finish();
             }
+        });
+
+        Button back = (Button) findViewById(R.id.Back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { finish(); }
         });
     }
 }
